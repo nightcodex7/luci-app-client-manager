40a235187472dbb1b80fe105050e397090c8d548
Tuhin Garai <tuhingarai123@gmail.com>
2026-08-04 00:30:01 +0530

    feat: add custom MAC address input option and implement input validation for WiFi management

--- htdocs/luci-static/resources/view/clientmanager/wifi.js
+++ htdocs/luci-static/resources/view/clientmanager/wifi.js
@@ -40,7 +40,7 @@ return view.extend({
 		var macSelect = E('select', {
 			'class': 'cbi-input-select cm-dropdown',
 			'id': 'cm-wifi-mac',
-			'style': 'min-width:300px;margin-bottom:16px;font-size:1em;padding:6px 10px;'
+			'style': 'min-width:300px;margin-bottom:8px;font-size:1em;padding:6px 10px;'
 		});
 
 		macSelect.appendChild(E('option', { 'value': '' },
@@ -50,6 +50,22 @@ return view.extend({
 				' (' + c.mac + ')';
 			macSelect.appendChild(E('option', { 'value': c.mac }, label));
 		});
+		macSelect.appendChild(E('option', { 'value': 'custom' },
+			_('✏️ Custom MAC Address...')));
+
+		var customMacInput = E('input', {
+			'type': 'text',
+			'class': 'cbi-input-text',
+			'id': 'cm-wifi-custom-mac',
+			'placeholder': 'AA:BB:CC:DD:EE:FF',
+			'style': 'display:none;width:220px;margin-left:10px;font-size:1em;padding:5px 8px;',
+			'maxlength': '17'
+		});
+
+		var deviceField = E('div', { 'class': 'cbi-value-field', 'style': 'display:flex;align-items:center;flex-wrap:wrap;' }, [
+			macSelect,
+			customMacInput
+		]);
 
 		// Group interfaces by radio/device
 		var byRadio = {};
@@ -94,12 +110,19 @@ return view.extend({
 			ifaceContainer.appendChild(radioDiv);
 		});
 
-		// Update checkboxes when device selection changes
-		macSelect.addEventListener('change', function() {
-			var mac = (this.value || '').toUpperCase();
+		function getActiveMac() {
+			if (macSelect.value === 'custom') {
+				return (customMacInput.value || '').trim().toUpperCase();
+			}
+			return (macSelect.value || '').trim().toUpperCase();
+		}
+
+		function updateCheckboxes() {
+			var mac = getActiveMac();
+			var isValid = /^([0-9A-FA-F]{2}[:-]){5}([0-9A-FA-F]{2})$/.test(mac);
 			var cbs = ifaceContainer.querySelectorAll('.cm-wifi-cb');
 			cbs.forEach(function(cb) {
-				if (!mac) {
+				if (!isValid) {
 					cb.disabled = true;
 					cb.checked = false;
 					return;
@@ -113,15 +136,29 @@ return view.extend({
 				} catch(e) {}
 				cb.checked = maclist.indexOf(mac) > -1;
 			});
+		}
+
+		// Update checkboxes when selection or custom MAC changes
+		macSelect.addEventListener('change', function() {
+			if (this.value === 'custom') {
+				customMacInput.style.display = 'inline-block';
+				customMacInput.focus();
+			} else {
+				customMacInput.style.display = 'none';
+			}
+			updateCheckboxes();
 		});
+
+		customMacInput.addEventListener('input', updateCheckboxes);
 
 		var applyBtn = E('button', {
 			'class': 'cbi-button cbi-button-save',
 			'click': function() {
-				var mac = macSelect.value;
-				if (!mac) {
+				var mac = getActiveMac();
+				var macRegex = /^([0-9A-FA-F]{2}[:-]){5}([0-9A-FA-F]{2})$/;
+				if (!mac || !macRegex.test(mac)) {
 					ui.addNotification(null,
-						E('p', {}, _('Please select a device.')), 'warning');
+						E('p', {}, _('Please enter or select a valid MAC address (e.g. AA:BB:CC:DD:EE:FF).')), 'warning');
 					return;
 				}
 
@@ -136,7 +173,7 @@ return view.extend({
 							.map(function(m) { return m.toUpperCase(); });
 					} catch(e) {}
 
-					var wasIn = maclist.indexOf(mac.toUpperCase()) > -1;
+					var wasIn = maclist.indexOf(mac) > -1;
 					var nowIn = cb.checked;
 
 					if (nowIn && !wasIn) {
@@ -154,7 +191,7 @@ return view.extend({
 
 				Promise.all(promises).then(function() {
 					ui.addNotification(null,
-						E('p', {}, _('MAC filter updated. WiFi is reloading…')),
+						E('p', {}, _('MAC filter updated for %s. WiFi is reloading…').format(mac)),
 						'info');
 					window.setTimeout(function() {
 						window.location.reload();
@@ -171,7 +208,7 @@ return view.extend({
 			E('div', { 'class': 'cbi-section' }, [
 				E('div', { 'class': 'cbi-value' }, [
 					E('label', { 'class': 'cbi-value-title', 'style': 'font-size:1.05em;font-weight:600;' }, _('Device')),
-					E('div', { 'class': 'cbi-value-field' }, macSelect)
+					deviceField
 				])
 			]),
 
